38da637eae8ef27d3ce905edb03d68c0d462ee57
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-16 20:47:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React,{Component} from 'react'
 import '../styles/App.css';
+class DigitalClock extends Component {
+  state = {
+    time: new Date()
+  };
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      time: new Date()
+    });
+  }
+
+  render() {
+    const { time } = this.state;
+    return (
+      <div>{time.toLocaleString()}</div>
+    );
+  }
+}
 const App = () => {
 
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">
+      <DigitalClock />
+      </div>
     </div>
   )
 }
